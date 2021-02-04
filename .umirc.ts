030b0a27a6842f74ff03ac75ce898b8c44d0c6cf
kennylbj <632786081@qq.com>
2021-02-04 18:42:33 +0800

    Add github page

--- .umirc.ts
+++ .umirc.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
+  base: '/product/',
+  publicPath: '/product/',
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [
-    { path: '/', component: '@/pages/index' },
-  ],
+  routes: [{ path: '/', component: '@/pages/index' }],
   fastRefresh: {},
 });
